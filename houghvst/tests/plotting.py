bd90dfb684e93aa3027dede0c2d048c04fc7aff6
marianotepper <Lulita75>
2017-09-18 15:44:45 -0400

    Code refactoring and improvement of plot_vst_accumulator_space

--- houghvst/tests/plotting.py
+++ houghvst/tests/plotting.py
@@ -1,12 +1,9 @@
 import colorcet as cc
-from matplotlib.colors import colorConverter, LinearSegmentedColormap
 import matplotlib.patches as mpatches
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import ImageGrid
 import numpy as np
-from scipy.ndimage import generic_filter1d
 import seaborn.apionly as sns
-from skimage.segmentation import find_boundaries
 import warnings
 
 
@@ -131,47 +128,45 @@ def plot_patch(patch, edgecolor=None, ax=None, vmin=None, vmax=None, cmap=None):
     return im_plot
 
 
-def plot_vst_accumulator_space(acc_space, cmap=cc.m_bgy, ax=None):
+def plot_vst_accumulator_space(acc_space, cmap=cc.m_fire, ax=None,
+                               plot_estimates=False, plot_focus=False):
     if ax is None:
         ax = plt.gca()
-
-
 
     alpha_step0 = acc_space.alpha_range[1] - acc_space.alpha_range[0]
     alpha_step1 = acc_space.alpha_range[-1] - acc_space.alpha_range[-2]
-    sigma_step0 = acc_space.sigma_range[1] - acc_space.sigma_range[0]
-    sigma_step1 = acc_space.sigma_range[-1] - acc_space.sigma_range[-2]
+    sigma_step0 = acc_space.sigma_sq_range[1] - acc_space.sigma_sq_range[0]
+    sigma_step1 = acc_space.sigma_sq_range[-1] - acc_space.sigma_sq_range[-2]
     im_plt = ax.imshow(acc_space.score, cmap=cmap,
                        extent=(acc_space.alpha_range[0] - alpha_step0 / 2,
                                acc_space.alpha_range[-1] + alpha_step1 / 2,
-                               acc_space.sigma_range[-1] + sigma_step1 / 2,
-                               acc_space.sigma_range[0] - sigma_step0 / 2)
+                               acc_space.sigma_sq_range[-1] + sigma_step1 / 2,
+                               acc_space.sigma_sq_range[0] - sigma_step0 / 2)
                        )
-    plt.axis('tight')
-    plt.xlabel(r'$\alpha$')
-    plt.ylabel(r'$\sigma$')
+    ax.axis('tight')
+    ax.set_xlabel(r'$\alpha$')
+    ax.set_ylabel(r'$\sigma$')
+    plt.colorbar(mappable=im_plt, ax=ax)
 
-    plt.colorbar(im_plt)
+    if plot_focus:
+        len_a = (acc_space.alpha_range[-1] - acc_space.alpha_range[0]) / 4
+        len_s = (acc_space.sigma_sq_range[-1]
+                 - acc_space.sigma_sq_range[0]) / 10
+        rect = mpatches.Rectangle((acc_space.alpha - len_a / 2,
+                                   acc_space.sigma_sq - len_s / 2),
+                                  len_a, len_s, ec='#0000cd', fc='none',
+                                  linewidth=3)
+        ax.add_artist(rect)
 
-
-def plot_slic_boundaries(img, slic_labels, color='r', normalize=False,
-                    ax=None):
-    if ax is None:
-        ax = plt.gca()
-    if normalize:
-        vmin, vmax = img.min(), img.max()
-    else:
-        vmin, vmax = 0, 255
-
-    ax.imshow(img, vmin=vmin, vmax=vmax, cmap='gray')
-
-    img_bounds = find_boundaries(slic_labels)
-    print(img.shape, img_bounds.shape)
-
-    color1 = colorConverter.to_rgba('black')
-    color2 = colorConverter.to_rgba(color)
-    cmap = LinearSegmentedColormap.from_list('my_cmap2', [color1, color2], N=2)
-    cmap._init()
-    cmap._lut[:, -1] = np.arange(5)
-
-    ax.imshow(img_bounds, cmap=cmap, cax=ax)
+    if plot_estimates:
+        tag_str = r'Estimated: $\alpha={:.2f}$, $\sigma={:.2f}$'
+        bbox_props = dict(boxstyle='round', fc='w', ec='#0000cd', alpha=0.5)
+        ax.annotate(tag_str.format(acc_space.alpha, acc_space.sigma_sq),
+                       xy=(acc_space.alpha, acc_space.sigma_sq), xycoords='data',
+                       xytext=(0.95, 0.05), textcoords='axes fraction',
+                       va='bottom', ha='right', color='#0000cd', size='large',
+                       bbox=bbox_props,
+                       arrowprops=dict(facecolor='#0000cd', edgecolor='none',
+                                       shrink=0., width=2, headwidth=3,
+                                       headlength=3)
+                       )

d0e03bd49c4450ca85bdccede9c050a891e6a054
marianotepper <Lulita75>
2018-03-26 15:21:04 -0400

    Show r2 score and minor plot modifications

--- houghvst/tests/test_phantom.py
+++ houghvst/tests/test_phantom.py
@@ -6,6 +6,7 @@ from mpl_toolkits.axes_grid1 import ImageGrid
 import numpy as np
 import seaborn.apionly as sns
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
 import tifffile
 from houghvst.estimation.estimation import compute_mean_var, \
     estimate_vst_movie, estimate_vst_image
@@ -29,6 +30,8 @@ def ground_truth_pixelwise(dir_name, files, box=None, plt_regression=True):
         fig, axes_scatter = plt.subplots(1, 1, figsize=(8, 4))
         means_means = []
         variances_means = []
+        all_means = []
+        all_variances = []
         for k, fn in enumerate(files):
             movie = tifffile.imread(dir_name + fn)
             if box is None:
@@ -44,18 +47,25 @@ def ground_truth_pixelwise(dir_name, files, box=None, plt_regression=True):
             means_means.append(means.mean())
             variances_means.append(variances.mean())
 
+            if plt_regression:
+                all_means.extend(means)
+                all_variances.extend(variances)
+
             axes_scatter.plot(means, variances, '.', alpha=0.7,
                               color=colors[k], markeredgecolor='none',
                               label='Movie {}'.format(k + 1), zorder=10 - k,
                               rasterized=True)
 
         if plt_regression:
+            all_means = np.array(all_means)[:, np.newaxis]
+            all_variances = np.array(all_variances)
+
             mod = LinearRegression()
-            mod.fit(np.array(means_means)[:, np.newaxis],
-                    np.array(variances_means))
-            x = np.array([1400, 2600])[:, np.newaxis]
+            mod.fit(all_means, all_variances)
+            x = np.array([all_means.min(), all_means.max()])[:, np.newaxis]
             axes_scatter.plot(x, mod.predict(x), 'k-', zorder=11)
             print('Linear fit:', mod.coef_[0], mod.intercept_)
+            print('R2 score', r2_score(all_variances, mod.predict(all_means)))
 
         for k, (mean, variance) in enumerate(zip(means_means, variances_means)):
             axes_scatter.scatter(mean, variance, marker='+',
@@ -180,55 +190,6 @@ def ground_truth_estimate_vst(dir_name, files, box=None, block_size=8,
         means, variances = compute_temporal_mean_var(movie_gat)
         print('Temporal variance MEAN={}, STD={}'.format(variances.mean(),
                                                          variances.std(ddof=1)))
-
-        # from skimage.measure import compare_psnr
-        # from skimage.restoration import denoise_nl_means, denoise_wavelet, denoise_tv_bregman, estimate_sigma
-        # from houghvst.estimation.gat import compute_gat
-        # img_gt_gat = compute_gat(img_gt, res.sigma, res.alpha)
-        # img_noisy_gat = compute_gat(img_noisy, res.sigma, res.alpha)
-        # data_range = np.array([0, 8191])
-        # data_range_gat = compute_gat(data_range, res.sigma, res.alpha)
-        # print(data_range_gat)
-        #
-        # print('sigma before GAT', estimate_sigma(img_noisy))
-        # print('sigma before GAT', estimate_sigma(img_noisy[80:120, 150:250]))
-        # print('sigma after GAT', estimate_sigma(img_noisy_gat))
-        # print('sigma before GAT', estimate_sigma(img_noisy_gat[80:120, 150:250]))
-        #
-        # print(img_noisy.min(), img_noisy.max())
-        # # img_noisy_clean = denoise_wavelet(img_noisy / data_range[1])
-        # # img_noisy_clean *= data_range[1]
-        # # img_noisy_gat_clean = denoise_wavelet(img_noisy_gat / data_range_gat[1])
-        # # img_noisy_gat_clean *= data_range_gat[1]
-        # img_noisy_clean = denoise_nl_means(img_noisy, h=1090)
-        # img_noisy_gat_clean = denoise_nl_means(img_noisy_gat, h=0.82)
-        #
-        # print(img_gt.dtype, img_noisy_clean.dtype)
-        # print(img_gt.min(), img_gt.max())
-        # print('PSNR before GAT',
-        #       compare_psnr(img_gt_gat, compute_gat(img_noisy_clean, res.sigma, res.alpha).astype(np.float32),
-        #                    data_range=data_range_gat[1] - data_range_gat[0]))
-        # print('PSNR after GAT',
-        #       compare_psnr(img_gt_gat, img_noisy_gat_clean.astype(np.float32),
-        #                    data_range=data_range_gat[1] - data_range_gat[0]))
-        #
-        # print((img_gt_gat - compute_gat(img_noisy_clean, res.sigma, res.alpha)).min(),
-        #       (img_gt_gat - compute_gat(img_noisy_clean, res.sigma, res.alpha)).max(),
-        #       ((img_gt_gat - compute_gat(img_noisy_clean, res.sigma, res.alpha)) ** 2).mean(),
-        #       (img_gt_gat - img_noisy_gat_clean).min(),
-        #       (img_gt_gat - img_noisy_gat_clean).max(),
-        #       ((img_gt_gat - img_noisy_gat_clean) ** 2).mean())
-        #
-        # fig, axes = plt.subplots(3, 3)
-        # axes[0, 0].imshow(img_noisy_gat)
-        # axes[0, 1].imshow(compute_gat(img_noisy_clean, res.sigma, res.alpha))
-        # axes[0, 2].imshow(img_gt_gat - compute_gat(img_noisy_clean, res.sigma, res.alpha))
-        # axes[1, 0].imshow(img_noisy_gat)
-        # axes[1, 1].imshow(img_noisy_gat_clean)
-        # axes[1, 2].imshow(img_gt_gat - img_noisy_gat_clean)
-        # axes[2, 0].imshow(img_noisy)
-        # axes[2, 1].imshow(img_gt)
-        # axes[2, 2].imshow(img_gt_gat - img_noisy)
 
         variances = []
         for i in range(len(movie)):
